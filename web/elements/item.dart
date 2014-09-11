30e48dad3f982584bc0467655b00db930be8da5d
Zero Gravitas <doublebranching@gmail.com>
2014-09-10 21:12:01 -0700

    Don't load images until after ready.

--- web/elements/item.dart
+++ web/elements/item.dart
@@ -10,6 +10,13 @@ class ProfiteerItem extends PolymerElement {
 
   factory ProfiteerItem() => new Element.tag('profiteer-item');
   ProfiteerItem.created() : super.created();
+
+  void ready() {
+    // This is kind of a weird hack, but it works. If you set the src in the
+    // template, then it blocks until the entire page has loaded, or
+    // something. Which looks ugly.
+    async((_) => shadowRoot.querySelector('img.icon').src = model.icon);
+  }
 }
 
 
