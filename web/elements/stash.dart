71a0f596c6c38b2c725d51083f1bd98ede13e516
Zero Gravitas <doublebranching@gmail.com>
2014-09-10 21:03:30 -0700

    Only have one league at a time in the DOM.
    This is really helpful for performance, especially on startup. Something
    isn't liking the whole 'load lots of images at once' thing.

--- web/elements/stash.dart
+++ web/elements/stash.dart
@@ -7,6 +7,7 @@ import '../src/model.dart';
 @CustomTag('profiteer-stash')
 class ProfiteerStash extends PolymerElement {
   @published Stash model;
+  @published int index = 0;
 
   factory ProfiteerStash() => new Element.tag('profiteer-stash');
   ProfiteerStash.created() : super.created();
