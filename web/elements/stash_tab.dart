fa3ad4b865be70d62ec608976172037f15f0362c
Zero Gravitas <doublebranching@gmail.com>
2014-09-11 19:06:29 -0700

    Inline profiteer-item.
    There's no point in having it since it's so simple, given that it only
    models the icon.

--- web/elements/stash_tab.dart
+++ web/elements/stash_tab.dart
@@ -3,6 +3,7 @@ library profiteer.elements.sync;
 import 'dart:html';
 import 'package:polymer/polymer.dart';
 import '../src/model.dart';
+import 'package:template_binding/template_binding.dart';
 
 @CustomTag('profiteer-stash-tab')
 class ProfiteerStashTab extends PolymerElement {
@@ -10,4 +11,19 @@ class ProfiteerStashTab extends PolymerElement {
 
   factory ProfiteerStashTab() => new Element.tag('profiteer-stash-tab');
   ProfiteerStashTab.created() : super.created();
+
+  void iconMouseover(Event e) {
+    asyncFire('item-mouseover',
+        detail: nodeBind(e.target).templateInstance.model.value);
+  }
+
+  void ready() {
+    // This is kind of a weird hack, but it works. If you set the src in the
+    // template, then it blocks until the entire page has loaded, or
+    // something. Which looks ugly.
+    shadowRoot.querySelectorAll('img.icon').forEach((img) {
+      Item model = nodeBind(img).templateInstance.model.value;
+      async((_) => img.src = model.icon);
+    });
+  }
 }
